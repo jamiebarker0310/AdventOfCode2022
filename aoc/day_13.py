25759a55b8df9c747d2b5d194af9c5b37d727ead
Jamie <dodgerbarker@gmail.com>
2022-12-13 21:41:46 +0000

    clean up docstring

--- aoc/day_13.py
+++ aoc/day_13.py
@@ -4,31 +4,7 @@ from functools import cmp_to_key
 
 def compare_lists(l1: list, l2: list) -> bool:
     """
-    compares list with the following criteria
-    If both values are integers, the lower integer should come first.
-    If the left integer is lower than the right integer,
-        the inputs are in the right order.
-    If the left integer is higher than the right integer,
-        the inputs are not in the right order.
-    Otherwise, the inputs are the same integer;
-        continue checking the next part of the input.
-
-    If both values are lists,
-        compare the first value of each list,
-        then the second value,
-        and so on.
-
-    If the left list runs out of items first,
-        the inputs are in the right order.
-    If the right list runs out of items first,
-        the inputs are not in the right order.
-
-    If the lists are the same length and no comparison makes a decision about the order,
-        continue checking the next part of the input.
-
-    If exactly one value is an integer,
-        convert the integer to a list which contains that integer as its only value,
-        then retry the comparison.
+    compare lists according to predefined rules
 
     Args:
         l1 (list): list which should be lower
