27ebfc12f39070d53717b894cc8f90606ec93b11
Jamie <dodgerbarker@gmail.com>
2022-12-17 22:23:52 +0000

    linting

--- aoc/day_15.py
+++ aoc/day_15.py
@@ -1,113 +1,160 @@
-from tqdm import tqdm
 import numpy as np
-import pandas as pd
 from collections import Counter
-
-def parse_line(line: str):
-
-    sensor = line[: line.index(":")]
-    sensor_x = int(sensor[sensor.index("x=") + 2 : sensor.index(",")])
-    sensor_y = int(sensor[sensor.index("y=") + 2 :])
-
-    beacon = line[line.index(":") + 1 :]
-    beacon_x = int(beacon[beacon.index("x=") + 2 : beacon.index(",")])
-    beacon_y = int(beacon[beacon.index("y=") + 2 :])
-
-    return [sensor_x + sensor_y * 1j, beacon_x + beacon_y * 1j]
+from functools import reduce
 
 
-def part_one(file_path: str, y=2_000_000) -> int:
-    """[summary]
+def parse_line(line: str) -> list:
+    """
+    parse string and return list of complex numbers
+    first representing sensor
+    second representing beacon
+
+    Args:
+        line (str): line
+
+    Returns:
+        list: list of sesnosr beacon
+    """
+    # initialise data
+    data = []
+    # for sensor and beacon
+    for substr in [line[: line.index(":")], line[line.index(":") + 1 :]]:
+        # find x and y
+        substr_x = int(substr[substr.index("x=") + 2 : substr.index(",")])
+        # multiple y by complex
+        substr_y = int(substr[substr.index("y=") + 2 :]) * 1j
+        # append to data
+        data.append(substr_x + substr_y)
+    # return data
+    return data
+
+
+def part_one(file_path: str, y: int = 2_000_000) -> int:
+    """
+    reads file
+    parses data
+    calculates difference between sensor and beacon
+
+
 
     Args:
         file_path (str): input file
+        y (int, optional): y value to check ruled out x points for.
+            Defaults to 2_000_000.
 
     Returns:
-        [type]: [description]
+        int: Number of x coordinates ruled out
     """
 
     # read file
     with open(file_path) as f:
         lines = f.readlines()
-
+    # parse files
     data = np.array([parse_line(line) for line in lines])
-
+    # calculates difference between sensor and beacon
     manhattan_dist = np.diff(data, axis=1)[:, 0]
     manhattan_dist = np.abs(np.real(manhattan_dist)) + np.abs(np.imag(manhattan_dist))
-
-    y_dist = np.abs(np.imag(data[:,0]) - y)
-
-    x_dist = np.vstack(manhattan_dist - y_dist).clip(min=0)[:,0]
-
-    x_range = np.vstack([np.real(data[:,0]) - x_dist, np.real(data[:,0]) + x_dist]).T.astype(int)
-
+    # calculate y distance
+    y_dist = np.abs(np.imag(data[:, 0]) - y)
+    # calculate x distance it can be
+    x_dist = np.vstack(manhattan_dist - y_dist).clip(min=0)[:, 0]
+    # calculate min and max x_val
+    x_range = np.vstack(
+        [np.real(data[:, 0]) - x_dist, np.real(data[:, 0]) + x_dist]
+    ).T.astype(int)
+    # initialise ruled out positions
     ruled_out = []
+    # for each row
     for row in x_range:
-        ruled_out.append(np.arange(start=row[0], stop=row[1]+1))
-    
+        # append the range between the two values
+        ruled_out.append(np.arange(start=row[0], stop=row[1] + 1))
+    # concatenate values
     ruled_out = np.concatenate(ruled_out)
+    # take unique of values
     ruled_out = np.unique(ruled_out)
-
-    beacons = np.real(data[:, 1])[np.where(np.imag(data[:, 1])==y)]
+    # remove all beacons on the row
+    beacons = np.real(data[:, 1])[np.where(np.imag(data[:, 1]) == y)]
     beacons = np.unique(beacons)
-        
     return len(ruled_out) - len(beacons)
 
-def part_two(file_path: str, max_val=4000000, n_rows=2):
-    """[summary]
+
+def part_two(file_path: str, max_val=4000000) -> int:
+    """
+    read file
+    parse data
+    calculate manhattan distnace between sensors and beacons
+    for each sensor, beacon calculate the outer perimeter
+    create set of perimeter points that occur at least 4 times
+    check whether each of these can be a beacon
 
     Args:
-        file_path (str): [description]
+        file_path (str): file path
+        max_val (int, optional): maximum value of ruled out point.
+        Defaults to 4000000.
 
     Returns:
-        [type]: [description]
+        int: 4000000 * x coordinate + y coordinate of ruled out point
     """
-
+    # read file
     with open(file_path) as f:
         lines = f.readlines()
-    
+    # parse data
     data = np.array([parse_line(line) for line in lines])
-
+    # calcaulte manhattan distances
     manhattan_dist = np.diff(data, axis=1)[:, 0]
     manhattan_dist = np.abs(np.real(manhattan_dist)) + np.abs(np.imag(manhattan_dist))
-
-    co_ords = []
+    # initialise counter object
+    counter = Counter()
+    # for each pair
     for i in range(len(data)):
         # calculate y values of perimeter
         y_range = np.arange(
-                        start = np.imag(data[i,0]) - manhattan_dist[i] - 1,
-                        stop= np.imag(data[i,0]) + manhattan_dist[i] + 2
-                        )
+            start=np.imag(data[i, 0]) - manhattan_dist[i] - 1,
+            stop=np.imag(data[i, 0]) + manhattan_dist[i] + 2,
+        )
         # calculate y distance for each of those
-        y_dist = np.abs(np.imag(data[i,0]) - y_range)
+        y_dist = np.abs(np.imag(data[i, 0]) - y_range)
         # calculate remaining x_dist for each value
         x_dist = manhattan_dist[i] + 1 - y_dist
         # minimum value and maximum_value
-        x_min = np.real(data[i,0]) - x_dist
-        x_max = np.real(data[i,0]) + x_dist
-
-        co_ords.append(np.concatenate([x_min, x_max]) + 1j * np.concatenate([y_range, y_range]))
-
-    co_ords = np.concatenate(co_ords)
-
-    co_ords = np.array([key for key, value in Counter(co_ords).items() if value >=4])
-    
-    co_ords = co_ords[np.where(
-        (np.real(co_ords) >= 0) & 
-        (np.real(co_ords) <= max_val) &  
-        (np.imag(co_ords) >= 0) & 
-        (np.imag(co_ords) <= max_val))]
-    
-    co_ords_grid, sensors = np.meshgrid(co_ords, data[:,0])
+        x_min = np.real(data[i, 0]) - x_dist
+        x_max = np.real(data[i, 0]) + x_dist
+        # get coords
+        co_ords = np.concatenate([x_min, x_max]) + 1j * np.concatenate(
+            [y_range, y_range]
+        )
+        # filter coordinates
+        co_ords = co_ords[
+            np.where(
+                reduce(
+                    lambda x, y: x & y,
+                    [
+                        np.real(co_ords) >= 0,
+                        np.real(co_ords) <= max_val,
+                        np.imag(co_ords) >= 0,
+                        np.imag(co_ords) <= max_val,
+                    ],
+                )
+            )
+        ]
+        # append all values
+        counter.update(
+            np.concatenate([x_min, x_max]) + 1j * np.concatenate([y_range, y_range])
+        )
+    # filter on all coords that occur 4+ times
+    co_ords = np.array([key for key, value in counter.items() if value >= 4])
+    # calculate distances from all beacons
+    co_ords_grid, sensors = np.meshgrid(co_ords, data[:, 0])
     coord_sensor_distance = co_ords_grid - sensors
-    coord_sensor_distance = np.abs(np.real(coord_sensor_distance)) + np.abs(np.imag(coord_sensor_distance))
-
+    coord_sensor_distance = np.abs(np.real(coord_sensor_distance)) + np.abs(
+        np.imag(coord_sensor_distance)
+    )
+    # get manhattan distance for each coord
     _, man_dist = np.meshgrid(co_ords, manhattan_dist)
-
+    # ensure all are outside manhattan distance
     free_space = co_ords[(coord_sensor_distance > man_dist).all(axis=0)][0]
-
-    return  np.real(free_space)*4000000 + np.imag(free_space)
-
+    # return calc value of free space
+    return np.real(free_space) * 4000000 + np.imag(free_space)
 
 
 if __name__ == "__main__":
--- tests/test_day_15.py
+++ tests/test_day_15.py
@@ -6,61 +6,52 @@ from aoc.day_15 import part_one, part_two, parse_line
 @pytest.mark.parametrize(
     "line,expected",
     [
-        (
-            "Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
-            [2+18j, -2+15j]
-        ),
-        (
-            "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
-            [9+16j, 10+16j]
-        ),
+        ("Sensor at x=2, y=18: closest beacon is at x=-2, y=15", [2 + 18j, -2 + 15j]),
+        ("Sensor at x=9, y=16: closest beacon is at x=10, y=16", [9 + 16j, 10 + 16j]),
         (
             "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
-            [13+2j, 15+3j],
+            [13 + 2j, 15 + 3j],
         ),
         (
             "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
-            [12+14j, 10+16j],
+            [12 + 14j, 10 + 16j],
         ),
         (
             "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
-            [10+20j, 10+16j],
+            [10 + 20j, 10 + 16j],
         ),
         (
             "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
-            [14+17j, 10+16j],
+            [14 + 17j, 10 + 16j],
         ),
         (
             "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
-            [8+7j, 2+10j],
+            [8 + 7j, 2 + 10j],
         ),
-        (
-            "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
-            [2, 2+10j]
-        ),
+        ("Sensor at x=2, y=0: closest beacon is at x=2, y=10", [2, 2 + 10j]),
         (
             "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
-            [0 + 11j, 2+10j],
+            [0 + 11j, 2 + 10j],
         ),
         (
             "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
-            [20+14j, 25+17j],
+            [20 + 14j, 25 + 17j],
         ),
         (
             "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
-            [17+20j, 21+22j],
+            [17 + 20j, 21 + 22j],
         ),
         (
             "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
-            [16+7j, 15+3j],
+            [16 + 7j, 15 + 3j],
         ),
         (
             "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
-            [14+3j, 15+3j],
+            [14 + 3j, 15 + 3j],
         ),
         (
             "Sensor at x=20, y=1: closest beacon is at x=15, y=3",
-            [20+1j, 15+3j],
+            [20 + 1j, 15 + 3j],
         ),
     ],
 )
@@ -80,5 +71,5 @@ def test_part_two():
 
     test_file_path = "tests/test_inputs/test_day_15.txt"
 
-    response = part_two(test_file_path, max_val=20) 
+    response = part_two(test_file_path, max_val=20)
     assert response == 56000011
